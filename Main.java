f657561d851146c9dd992ab845ce3261c9d684d7
noahlattari <zennoah1998@gmail.com>
2020-10-04 21:23:29 -0700

    More comments

--- Main.java
+++ Main.java
@@ -13,19 +13,19 @@ class Main {
     public static void main(String[] args) {
         File currentFile;
 
-        JFileChooser chooser = new JFileChooser();
+        JFileChooser chooser = new JFileChooser(); // Java UI to pick a file
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Java files", "java");
         chooser.setFileFilter(filter);
         chooser.setDialogTitle("Choose a file to analyze");
-        int returnVal = chooser.showOpenDialog(null);
+        int returnVal = chooser.showOpenDialog(null); // Determines if frame opened proeprly
 
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             currentFile = chooser.getSelectedFile();
             CodeAnalyzer analyzer = new CodeAnalyzer();
             String result = analyzer.countLines(currentFile);
-            System.out.println(result);
-        } else if (returnVal == JFileChooser.CANCEL_OPTION) {
-            System.exit(0);
+            System.out.println(result); // Reulting output in command line
+        } else if (returnVal == JFileChooser.CANCEL_OPTION) { // If the user closes the frame
+            System.exit(0); // Exit the application so app doesn't use any more resources
         }
 
     }
