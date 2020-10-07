fb01085b16bf9f25b7542b1f3605bb241ce16526
noahlattari <zennoah1998@gmail.com>
2020-10-06 22:48:56 -0700

    added more testing files

---
+++ examples/EdgeCases.java
@@ -0,0 +1,21 @@
+package examples;
+
+class EdgeCases {
+    // Welcome to the test zone
+    String dummy = "test// /* */"; // Hi /* */ TODO
+    /*
+     * TODO
+     * 
+     * x
+     * 
+     */
+    int x = 1;
+    /** TODO */
+    int /* dd */ y = 4;
+    String /* ee */ z = "ye//s";
+    String o = "\"TODO\"";
+    /*
+     * //TODO
+     * 
+     */
+}
